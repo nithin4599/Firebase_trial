c7fda0b778caa2af106ff74c1564917e02b61971
Nithin <64428380+nithin4599@users.noreply.github.com>
2023-03-10 06:06:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Your web app's Firebase configuration
 
 var firebaseConfig = {
-    apiKey: "AIzaSyA1Eu8xyx7OAQ8y4ng-Fu6phrCWJWHiYlc",
-    authDomain: "trial-b04e6.firebaseapp.com",
-    databaseURL: "https://trial-b04e6-default-rtdb.firebaseio.com",
-    projectId: "trial-b04e6",
-    storageBucket: "trial-b04e6.appspot.com",
-    messagingSenderId: "870551973559",
-    appId: "1:870551973559:web:151d8cb3f567a01109e732",
-    measurementId: "G-HM63X09XYF"
+    apiKey: "Your api key here",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
